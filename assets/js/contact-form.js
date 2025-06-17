1fbb5bf86ddeb22951f91f8db887379e9766c6d6
Lili <fjzll.nju@gmail.com>
2025-06-17 14:13:29 +0800

    feat(contact-page): add js to validate user input in contact form

---
+++ assets/js/contact-form.js
@@ -0,0 +1,74 @@
+// Validation the user input in Contact Us form
+
+const button = document.getElementById("submit");
+
+// Regex patterns
+const namePattern = /^[a-zA-Z\s'-]{2,}$/;
+const emailPattern = /^[\w.-]+@[\w.-]+\.\w{2,}$/;
+
+
+// Event trigger when submit button is clicked
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    // Get filed values
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    // Clear error messages
+    document.getElementById("nameError").textContent = "";
+    document.getElementById("emailError").textContent = "";
+    document.getElementById("messageError").textContent = "";
+
+    // set the validUserInput to true
+    let validUserInput = true;
+
+    //Validate name
+    if (!namePattern.test(name)){
+        document.getElementById("nameError").textContent = "Please enter a valid name";
+        validUserInput = false;
+        return;
+    }
+
+    //Validate email
+    if (!emailPattern.test(email)||!email){
+        document.getElementById("emailError").textContent = "Please enter a valid email";
+        validUserInput = false;
+        return;
+    }
+
+    //Validate message
+    if (!message){
+        document.getElementById("messageError").textContent = "Message cannot be empty";
+        validUserInput = false;
+        return;
+    }
+
+    if (validUserInput){
+
+        // Create a user input object
+        const userInputData = {
+            name,
+            email,
+            message,
+        }
+
+        //Get submissions from localStorage or create a new array
+        let submissions = JSON.parse(localStorage.getItem("submissions")) || [];
+
+        //Add new submission
+        submissions.push(userInputData);
+
+        localStorage.setItem("submissions", JSON.stringify(submissions));
+
+        alert("Your message has been sent successfully!");
+
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+
+    }
+
+})
+
